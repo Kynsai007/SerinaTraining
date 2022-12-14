008f912c35e33207e5370c2ab10ea4ac5522eff6
Kynsai Iangrai <kynsai.iangrai@datasemantics.in>
2022-12-14 16:33:12 +0530

    changes

--- Utility-tool/utilityBase/src/app/utility/components/fr-update/fr-update.component.ts
+++ Utility-tool/utilityBase/src/app/utility/components/fr-update/fr-update.component.ts
@@ -491,14 +491,17 @@ export class FrUpdateComponent implements OnInit,AfterContentInit {
 
   updateMetainfo(value) {
    if((this.templateKeys !='') && this.updateMetaData.valid){
-    for(let key of Object.keys(this.allsynonyms)){
-      for(let s of this.templateKeys){
-        if(key != this.vendorName && this.allsynonyms[key].includes(s)){
-          alert(`Duplicate Synonym found for vendor ${key}, Synonym: ${s}, Please remove it and retry!`);
-          return;
+    if(this.allsynonyms && Object.keys(this.allsynonyms).length > 0){
+      for(let key of Object.keys(this.allsynonyms)){
+        for(let s of this.templateKeys){
+          if(key != this.vendorName && this.allsynonyms[key].includes(s)){
+            alert(`Duplicate Synonym found for vendor ${key}, Synonym: ${s}, Please remove it and retry!`);
+            return;
+          }
         }
       }
-    }
+    } 
+    
     if(value['FolderPath'] == ''){
       value['FolderPath'] = (<HTMLInputElement>document.getElementById("FolderPath")).value;
     }
