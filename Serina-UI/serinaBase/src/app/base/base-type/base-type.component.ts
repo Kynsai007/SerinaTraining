b742f28f79004319e0f1c96ff0943c400a6da6a2
Kynsai Iangrai <kynsai.iangrai@datasemantics.co>
2024-03-17 23:52:52 +0530

    ui changes

--- Serina-UI/serinaBase/src/app/base/base-type/base-type.component.ts
+++ Serina-UI/serinaBase/src/app/base/base-type/base-type.component.ts
@@ -131,12 +131,16 @@ export class BaseTypeComponent implements OnInit, OnDestroy,AfterViewInit {
   getMoreText(){
     if(this.router.url.includes('documentSummary')){
       this.more_text = 'Summary';
-    } else if(this.router.url.includes('vendor')){
+    } else if(this.router.url.includes('/customer/vendor/')){
       this.more_text = this.supplier_names;
-    } else if(this.router.url.includes('vendor')){
+    } else if(this.router.url.includes('roles')){
       this.more_text = 'Roles';
+    } else if(this.router.url.includes('service_batch_trigger')){
+      this.more_text = 'Service batch trigger';
+    } else if(this.router.url.includes('bulkUploadService')){
+      this.more_text = 'Bulk Upload';
     } else {
-      this.more_text = 'More';
+      this.more_text = "More"
     }
   }
   appendScript (){
@@ -166,7 +170,9 @@ export class BaseTypeComponent implements OnInit, OnDestroy,AfterViewInit {
     sessionStorage.clear();
   }
   openDialog() {
-    this.dialog.open(ChangePasswordComponent);
+    const drf:MatDialogRef<ChangePasswordComponent> = this.dialog.open(ChangePasswordComponent,{ 
+      width : '450px',
+      hasBackdrop: false})
   }
   readConfig(){
     this.settingService.readConfig().subscribe((data:any)=>{
