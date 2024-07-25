8830cd3397bbe28baf80b4ed91b3585c91c28fee
Kynsai Iangrai <kynsai_iangrai@owfg.com>
2024-07-25 12:33:10 +0530

    ui changes

--- Serina-UI/serinaBase/src/environments/environment.prod.ts
+++ Serina-UI/serinaBase/src/environments/environment.prod.ts
@@ -2,15 +2,15 @@
 
 export const environment = {
   production: true,
-  apiUrl: `https://${location.href.split("https://")[1].split(".serinaplus.com")[0]}.centralindia.cloudapp.azure.com`,
-  // apiUrl:"http://127.0.0.1:8000",
+  //apiUrl: `https://${location.href.split("https://")[1].split(".serinaplus.com")[0]}.centralindia.cloudapp.azure.com`,
+  apiUrl:"http://127.0.0.1:8000",
   apiVersion: "apiv1.1",
   userData: JSON.parse(localStorage.getItem('currentLoginUser')),
   userName: JSON.parse(localStorage.getItem('username'))
 };
 
 export const environment1  = {
-  hostname: `${location.href.split("https://")[1].split(".serinaplus.com")[0]}.centralindia.cloudapp.azure.com/apiv1.1`,
+  hostname: `enovadev.centralindia.cloudapp.azure.com/apiv1.1`,
   // port: 443,
   // protocol: 'wss',
   // path: '/console',
