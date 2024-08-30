523d4ce8cb25a656776d20d55d35778217b8622d
Lakshmiprathap Vadde <lakshmiprathap.v@datasemantics.in>
2024-08-30 10:34:26 +0530

    enova logout bug fixes

--- Serina-UI/serinaBase/src/app/base/base-type/base-type.component.ts
+++ Serina-UI/serinaBase/src/app/base/base-type/base-type.component.ts
@@ -16,6 +16,7 @@ import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import { ChangePasswordComponent } from '../change-password/change-password.component';
 import { FeatureComponent } from '../feature/feature.component';
 import { ConfirmationComponent } from '../confirmation/confirmation.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-base-type',
@@ -98,7 +99,8 @@ export class BaseTypeComponent implements OnInit, OnDestroy,AfterViewInit {
     private authService: AuthenticationService,
     private serviceProviderService : ServiceInvoiceService,
     public dialog: MatDialog,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private titleService: Title
   ) {
     this.subscription1 = this.SharedService.getMessage().subscribe(
       (message) => {
@@ -115,6 +117,11 @@ export class BaseTypeComponent implements OnInit, OnDestroy,AfterViewInit {
     if(this.dataStoreService.configData.client_name == 'Cenomi'){
       this.logoSrc = 'assets/Serina Assets/new_theme/cenomiLogo.png';
       this.dataStoreService.changeTheme("#20113E",'#ffffff');
+    } else if(this.dataStoreService?.configData?.client_name == 'AGI'){
+      this.titleService.setTitle('Finance Shared Service Invoice Portal');
+      this.setFavicon('assets/Serina Assets/new_theme/AGI/fav_AGI.png');
+      this.dataStoreService.changeTheme("#482464",'#ffffff');
+      this.logoSrc = 'assets/Serina Assets/new_theme/AGI/agi_home.png';
     } else {
       this.dataStoreService.changeTheme("#358dc0",'#140101');
       this.logoSrc = 'assets/Serina Assets/new_theme/logo.png';
@@ -135,6 +142,21 @@ export class BaseTypeComponent implements OnInit, OnDestroy,AfterViewInit {
     // this.readVendorNames();
   }
 
+  setFavicon(iconUrl: string) {
+    const link: HTMLLinkElement = this.renderer.createElement('link');
+    link.rel = 'icon';
+    link.href = iconUrl;
+
+    const head = document.getElementsByTagName('head')[0];
+    const oldLink = document.querySelector("link[rel*='icon']");
+    
+    if (oldLink) {
+      this.renderer.removeChild(head, oldLink);
+    }
+    
+    this.renderer.appendChild(head, link);
+  }
+  
   ngAfterViewInit(): void {
     window.onbeforeunload = () => {
       // Clear the session storage item when the page is reloaded
@@ -525,13 +547,6 @@ export class BaseTypeComponent implements OnInit, OnDestroy,AfterViewInit {
   }
   doc_status_route(){
     this.navClick();
-    if(this.vendorInvoiceAccess && this.ap_boolean){
-      this.router.navigate([`${this.portalName}/invoice/allInvoices`]);
-    } else if(this.vendorInvoiceAccess && !this.ap_boolean){
-      this.router.navigate([`${this.portalName}/invoice/PO`]);
-    } else{
-      this.router.navigate([`${this.portalName}/invoice/ServiceInvoices`]);
-    }
   }
   exceptionDrop(){
     this.isOpen_apr = false;
